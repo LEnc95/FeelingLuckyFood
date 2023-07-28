85bc5a9e886a3629db6a5d2e35827921dc7facdd
Luke Encrapera <l.encrapera@gmail.com>
2023-07-28 13:06:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,64 @@ const breakfastOptions = [
     "Egg McMuffin",
     "Hotcakes and Sausage",
     "Sausage Burrito",
-    // Add more breakfast options here...
+    "Fruit 'N Yogurt Parfait",
+    "Fruit & Maple Oatmeal",
+    "Bacon, Egg & Cheese McGriddles",
+    "Sausage McMuffin with Egg",
+    "Hotcakes",
+    "Peanut Butter Overnight Oats",
+    "Blueberry Muffins",
+    "Greek Yogurt with Berries",
+    "Chia Seed Pudding",
+    "Smoothie Bowl with Granola",
+    "Breakfast Burrito",
+    "Avocado Toast",
+    "Banana Pancakes",
+    "Acai Bowl",
   ];
   
   const lunchOptions = [
     "Big Mac",
     "Quarter Pounder with Cheese",
     "McChicken",
-    // Add more lunch options here...
+    "Filet-O-Fish",
+    "Double Cheeseburger",
+    "Chicken McNuggets (10 pc)",
+    "Crispy Chicken Sandwich",
+    "Premium Southwest Salad",
+    "Bacon, Egg & Cheese Biscuit",
+    "Hash Brown Scramble Bowl",
+    "Chicken Caesar Salad",
+    "Grilled Chicken Wrap",
+    "Classic Sausage, Egg & Cheese",
+    "Asiago Ranch Chicken Club",
+    "Spicy Chicken Sandwich",
+    "Son of Baconator",
+    "Spicy Chicken Go-Wrap",
+    "Bacon Jalapeño Chicken Sandwich",
   ];
   
   const dinnerOptions = [
     "Double Quarter Pounder with Cheese",
     "McDouble",
     "Artisan Grilled Chicken Sandwich",
-    // Add more dinner options here...
+    "Filet-O-Fish",
+    "Buttermilk Crispy Chicken Sandwich",
+    "Crispy Chicken Sandwich",
+    "Quarter Pounder with Cheese",
+    "Premium Bacon Ranch Salad",
+    "Bacon Jalapeño Cheeseburger",
+    "Homestyle Chicken Biscuit",
+    "Sushi Rolls",
+    "Grilled Vegetable Salad",
+    "Dave's Single",
+    "Classic Chicken Nuggets (6 pc)",
+    "Grilled Chicken Sandwich",
+    "Spicy Chicken Sandwich",
+    "Grilled Market Salad",
+    "Chick-fil-A Deluxe Sandwich",
   ];
-  
+    
   // Function to get a random item from an array
   function getRandomItemFromArray(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
